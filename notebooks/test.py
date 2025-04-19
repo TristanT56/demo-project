cc01edb1a2aac86b96be7ad98451fd67c22fcba8
Tristan Thomas <tristanthomas@air-de-tristan.home>
2025-04-20 00:15:08 +0200

    erz

---
+++ notebooks/test.py
@@ -0,0 +1,13 @@
+def my_function(x, y):
+    if x > y:
+        print("x est plus grand que y")
+    else:
+        print("x est plus petit ou égal à y")
+
+
+def another_function(z):
+    print(f"La valeur de z est: {z}")
+
+
+for i in range(10):
+    print(i)
